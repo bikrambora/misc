f4a0c7b2fea819a7c511074ea7cfe33a4fd64402
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-10-16 18:35:12 +1300

    all fhir code done

--- fhir-graphql/src/index.js
+++ fhir-graphql/src/index.js
@@ -17,7 +17,7 @@ import {
   GraphQLInterfaceType
 } from 'graphql';
 
-// Generic
+// Generic section
 const trace = (msg) => R.tap(x => console.log(msg, x));
 const notNil = R.compose(R.not, R.isNil);
 const greaterThan = R.flip(R.gt);
@@ -29,7 +29,18 @@ const capitalize = str => R.compose(
   R.compose(R.toUpper, R.head)
 )(str);
 
-// FHIR
+// FHIR section
+const structureCode = {
+  "path":"code",
+  "short":"Primitive Type id",
+  "definition":"Short code (no def)",
+  "comments":"RFC 4122",
+  "min":0,
+  "max":"*",
+  "base":{"path":"string","min":0,"max":"*"},
+  "type":[{"code":"Element"}],
+  "children":["code.extension","code.value","code.id"]
+};
 const toFieldName = R.compose(R.replace(/-/g, "_"), R.path(['resource', 'name']));
 const whenUnderscore = R.when(R.compose(R.equals('_'), R.head), R.slice(1, R.Infinity));
 const zipWithfieldName = R.curry( (x, y) => ({ resource: { ...x.resource, fieldName: y } }) );
@@ -43,9 +54,10 @@ const schemaItemHasElement = R.compose(
   R.both(notNil, lengthGreaterThan(1)),
   R.path(['resource', 'snapshot', 'element'])
 );
+const pathPrefix = R.compose(R.join("."), R.slice(0, -1), R.split("."));
 const whenPathHasX = R.when(
   R.compose(R.test(/\[x\]/), R.prop('path')),
-  R.compose((x) => x[1], (e) => R.mapAccum((a, b) => {
+  R.compose(R.indexOf(1), (e) => R.mapAccum((a, b) => {
     const transformations = {
       path: R.replace("[x]", capitalize(b.code)),
       type: [b]
@@ -55,6 +67,7 @@ const whenPathHasX = R.when(
 );
 
 const importStructure = R.compose(
+  R.assoc('code', structureCode),
   R.reduce((a, b) => R.assoc(b.path, b)(a), {}),
   R.chain(whenPathHasX),
   R.chain(R.path(['resource', 'snapshot', 'element'])),
@@ -62,8 +75,48 @@ const importStructure = R.compose(
   R.prop('entry')
 );
 
-//console.log(searchParameters.entry[0]);
-console.log(R.mergeAll([importStructure(profileTypes), importStructure(profileResources)]));
+const addChildren = structure => {
+  const cloned = R.clone(structure);
+  return R.compose(
+    R.always(cloned),
+    R.map(k => {
+      const path = k.path;
+      const prefix = pathPrefix(path);
+      if(!cloned[prefix]) return k;
+      cloned[prefix].children = cloned[prefix].children || [];
+      cloned[prefix].children.push(path);
+      return k;
+    }),
+    R.filter(
+      R.compose(
+        R.either(
+          R.compose(R.not, R.test(/\.contained$/)),
+          R.compose(R.not, R.equals(0), R.length, pathPrefix)
+        ),
+        R.prop('path')
+      )
+    )
+  )(cloned);
+};
+
+const allResourcesNames = structure => R.compose(R.filter(k => structure[k].type && structure[k].type[0].code === "DomainResource"), R.keys)(structure);
+const resourceFromUri = R.compose(R.last, R.split('/'));
+const namesToPaths = structure => R.compose(
+  R.reduce((acc, item) => R.assoc(item[0], item[1], acc), {}),
+  R.map(k => [structure[k].path.split(".")[0] + " " + structure[k].name, structure[k].path]),
+  R.filter(k => R.path([k, 'name'], structure)),
+  R.keys
+)(structure);
+
+// Next makeSchema
+// GraphQl section
+
+console.log(
+  R.compose(
+    addChildren,
+    R.mergeAll
+  )([importStructure(profileTypes), importStructure(profileResources)])
+);
 
 // import express from 'express';
 // import graphqlHTTP from 'express-graphql';
