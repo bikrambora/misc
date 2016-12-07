5ecf9f6ff205309b5598846e1f65f5e2b5dad03e
Lucas Schejtman <lucas.schejtman@gmail.com>
2016-12-08 07:42:14 +1300

    more charts

--- ml/nba.py
+++ ml/nba.py
@@ -2,9 +2,24 @@ import pandas
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+
 nba = pandas.read_csv("nba_2013.csv")
 
-sns.pairplot(nba[["ast", "fg", "trb"]])
+kmeans_model = KMeans(n_clusters=5, random_state=1)
+good_columns = nba._get_numeric_data().dropna(axis=1)
+kmeans_model.fit(good_columns)
+labels = kmeans_model.labels_
+
+pca_2 = PCA(2)
+plot_columns = pca_2.fit_transform(good_columns)
+plt.scatter(x=plot_columns[:,0], y=plot_columns[:,1], c=labels)
 plt.show()
+
+# nba = pandas.read_csv("nba_2013.csv")
+#
+# sns.pairplot(nba[["ast", "fg", "trb"]])
+# plt.show()
 
 print nba.mean()
