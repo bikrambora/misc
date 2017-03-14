fcea2fd00df1fbe3281ce519fbd415cc2fc7e8ed
Lucas <lucas.schejtman@gmail.com>
2017-03-15 12:07:25 +1300

    initial deps script

---
+++ deps/index.js
@@ -0,0 +1,51 @@
+const R = require('ramda');
+const future = require('fluture');
+const request = require('request');
+const semver = require('semver');
+const semverUtils = require('semver-utils');
+
+const createRow = R.curry((name, version, type, dep, depVersion) => `${name}, ${version}, ${type}, ${dep}, ${depVersion}, ${versionToRange(depVersion)}`);
+
+const pkg = {
+  "name": "ohw-svg",
+  "version": "1.1.0",
+  "dependencies": {
+    "gsap": "^1.19.0",
+    "lodash": "^3.10.1",
+    "react": "^15.0.1",
+    "react-dom": "^15.0.1"
+  },
+  "devDependencies": {
+    "babel-plugin-transform-class-properties": "^6.4.0",
+    "babel-plugin-transform-es2015-classes": "^6.4.0",
+    "babel-preset-es2015": "^6.6.0",
+    "babel-preset-react": "^6.3.13",
+    "chai": "^3.4.1",
+    "eslint": "2.5.3",
+    "eslint-config-orionhealth": "~2.0.1",
+    "javascript-workflow-orionhealth": "^0.6.1",
+    "karma-junit-reporter": "^0.4.1",
+    "oh-roost-plugin-hive": "*",
+    "react-addons-test-utils": "^15.0.1",
+    "skin-deep": "^0.16.0"
+  }
+};
+
+const peek = R.tap(console.log);
+const propToPairs = R.curry((name, obj) => R.compose(R.toPairs, R.prop(name))(obj));
+const pairsToRows = R.curry((row, pairs) => R.map(arr => row(arr[0], arr[1]))(pairs));
+const objToRows = R.curry((prop, row, obj) => R.compose(pairsToRows(row), propToPairs(prop))(obj));
+
+const versionToRange = (version) => {
+    const range = semverUtils.parseRange(version)[0];
+    return range ? `${R.join(',', [range.major, range.minor, range.patch])}, ${range.operator ? range.operator : 'fixed'}` : `any, *`;
+};
+
+const createAppCsv = (pJson) => {
+  const nameAndVersionRow = createRow(pJson.name, pJson.version);
+  const dependenciesRows = objToRows('dependencies', nameAndVersionRow('runtime'));
+  const devDependenciesRows = objToRows('devDependencies', nameAndVersionRow('development'));
+  return R.converge(R.concat, [dependenciesRows, devDependenciesRows])(pJson);
+};
+
+console.log(createAppCsv(pkg));
