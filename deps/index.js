b0cd2e4b5d0f1c42c0d287d952923ef338f2f683
Lucas <lucas.schejtman@gmail.com>
2017-03-16 10:46:02 +1300

    added saving to file

--- deps/index.js
+++ deps/index.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
 const R = require('ramda');
+const { fromNullable: maybe } = require('data.maybe');
 const Future = require('fluture');
 const request = require('request');
-const semver = require('semver');
-const semverUtils = require('semver-utils');
+const semver = require('semver-utils');
+
+const header = 'name,version,type,dependency, depversion, depversionclean, depversionoperator';
+
+const fetch = (opts) => Future((rej, res) => void request(opts, (err, response, body) => {
+    if(err) return rej(err);
+    return res(body);
+}));
+
+const writeFile = R.curry((file, contents) => fs.writeFileSync(file, contents));
 
 const pkg = {
   "name": "ohw-svg",
@@ -39,26 +49,23 @@ const propToPairs   = R.curry((name, obj) => R.compose(R.toPairs, R.prop(name))(
 const pairsToRows   = R.curry((row, pairs) => R.map(arr => row(arr[0], arr[1]))(pairs));
 const objToRows     = R.curry((prop, row, obj) => R.compose(pairsToRows(row), propToPairs(prop))(obj));
 const pkgUrl        = R.curry((repo) => ({ uri:`https://stash/projects/OHW/repos/${repo}/browse/package.json?raw`, rejectUnauthorized: false }));
-const createRow     = R.curry((name, version, type, dep, depVersion) => `${name}, ${version}, ${type}, ${dep}, ${depVersion}, ${versionToRange(depVersion)}`);
+const createRow     = R.curry((name, version, type, dep, depVersion) => `${name},${version},${type},${dep},${depVersion},${versionToRange(depVersion)}`);
 const fetchPackages = R.compose(R.map(fetch), R.map(pkgUrl))(packages);
 
 const versionToRange = (version) => {
-    const range = semverUtils.parseRange(version)[0];
-    return range ? `${R.join(',', [range.major, range.minor, range.patch])}, ${range.operator ? range.operator : 'fixed'}` : `any, *`;
+    const range = maybe(semver.parseRange(version)[0]);
+    const clean = range.map(R.compose(R.join('.'), R.props(['major', 'minor', 'patch']))).getOrElse('any');
+    const operator = range.chain(R.compose(maybe, R.prop('operator'))).getOrElse('fixed');
+    return R.join(',', [clean, operator]);
 };
 
-const fetch = (opts) => Future((rej, res) => void request(opts, (err, response, body) => {
-    if(err) return rej(err);
-    return res(body);
-}));
-
 const createAppCsv = (pJson) => {
-  const nameAndVersionRow = createRow(pJson.name, pJson.version);
-  const dependenciesRows = objToRows('dependencies', nameAndVersionRow('runtime'));
-  const devDependenciesRows = objToRows('devDependencies', nameAndVersionRow('development'));
-  return R.converge(R.concat, [dependenciesRows, devDependenciesRows])(pJson);
+    const nameAndVersionRow = createRow(pJson.name, pJson.version);
+    const dependenciesRows = objToRows('dependencies', nameAndVersionRow('runtime'));
+    const devDependenciesRows = objToRows('devDependencies', nameAndVersionRow('development'));
+    return R.converge(R.concat, [dependenciesRows, devDependenciesRows])(pJson);
 };
 
 Future.parallel(Infinity, fetchPackages)
     .chain(Future.encase(R.map(JSON.parse)))
-    .fork(console.log, R.compose(peek, R.map(createAppCsv)));
+    .fork(console.log, R.compose(writeFile('./alldeps.csv'), R.join('\n'), R.prepend(header), R.flatten, R.map(createAppCsv)));
