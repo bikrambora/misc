8e25178cf2b316204d8f5dd10891a89b5d94e69f
Lucas <lucas.schejtman@gmail.com>
2017-03-28 13:06:36 +1300

    starting package.json creating work

--- generator-shell/app/index.js
+++ generator-shell/app/index.js
@@ -3,12 +3,49 @@ const Generator = require('yeoman-generator');
 class AppGenerator extends Generator {
     constructor(args, opts) {
         super(args, opts);
-
-        this.option('babel');
     }
 
-    method1() {
-        this.log('method 1 running');
+    async prompting() {
+        this.prompts = await this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'Your component name',
+                default: 'shell-component'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Your component description',
+                default: 'awesome piece of react'
+            },
+            {
+                type: 'checkbox',
+                name: 'deps',
+                message: 'Select extra dependencies',
+                choices: [ 'redux', 'ohw-svg' ]
+            }
+        ]);
+    }
+
+    writing() {
+        this.fs.copyTpl(
+            this.templatePath('package.ejs'),
+            this.destinationPath('package.json'),
+            {
+                prompts: this.prompts
+            }
+        );
+    }
+
+    install() {
+        this.installDependencies(
+            {
+                npm: true,
+                bower: false,
+                yarn: false
+            }
+        );
     }
 }
 
