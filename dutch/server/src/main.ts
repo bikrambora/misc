7daf2b8b6e5d8d1cc624e55c6ce8070eb8452292
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-04-09 18:00:25 +1200

    added debug support

--- dutch/server/src/main.ts
+++ dutch/server/src/main.ts
@@ -1,17 +1,18 @@
 // Below is an example of using TSLint errors suppression
 
 /**
- * Returns a Promise<string> that resolves after given time.
+ * Returns a string that resolves after given time.
  *
  * @param {string} name - Somebody's name
- * @param {number=} [delay=2000] - Number of milliseconds to delay resolution.
- * @returns {Promise<string>}
+ * @returns {string}
  */
-function delayedHello(name: string, delay: number = 2000): Promise<string> {
+function delayedHello(name: string): string {
   // tslint:disable-next-line no-string-based-set-timeout
-  return new Promise((resolve) => setTimeout(resolve(`Hello, ${name}`), delay));
+  return `Hello, ${name}`;
 }
 
 export default async function greeter(name) { // tslint:disable-line
   return await delayedHello(name);
 }
+
+greeter('some name');
