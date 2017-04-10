7e738e759349c65149aba5a0568e2e94cc22fa8c
Lucas <lucas.schejtman@gmail.com>
2017-04-11 11:01:45 +1200

    can filter and select type

--- vscode/typesearch/src/extension.ts
+++ vscode/typesearch/src/extension.ts
@@ -1,29 +1,48 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+
+import types from './types';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+interface QuickItem {
+    description: string;
+    label: string;
+    detail: string;
+};
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "typesearch" is now active!');
+interface RawTypeDefinition {
+	t: string;
+	g: string[];
+	m: string[];
+	p: string;
+	l: string;
+	d: number;
+};
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
+const placeHolder = 'Search for Types Packages';
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+function onTypeSelected(selected) {
+    const item = selected as QuickItem;
+    console.log(item);
+}
+
+function typesToItems(types: RawTypeDefinition[]) : QuickItem[] {
+    return types
+    .map((type) => ({ description: type.l, label: type.t, detail: type.p } as QuickItem))
+    .sort((a, b) => {
+        if(a.label < b.label) return -1;
+        if(a.label > b.label) return 1;
+        return 0;
     });
-
-    context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
+async function onCommand() {
+    const selected = await vscode.window.showQuickPick(typesToItems(types), { placeHolder });
+    onTypeSelected(selected);
 }
+
+export function activate(context: vscode.ExtensionContext) {
+    const searchTypeSearch = vscode.commands.registerCommand('extension.typesearch', onCommand);
+    context.subscriptions.push(searchTypeSearch);
+}
+
+export function deactivate() { }
