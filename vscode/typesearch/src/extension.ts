a9cefc0664fb61a50cfb4e9bd75960a5f20ee999
Lucas <lucas.schejtman@gmail.com>
2017-04-12 13:18:11 +1200

    tidy sort fn

--- vscode/typesearch/src/extension.ts
+++ vscode/typesearch/src/extension.ts
@@ -20,22 +20,18 @@ interface RawTypeDefinition {
 
 const placeHolder = 'Search for Types Packages';
 
-function onTypeSelected(selected: QuickItem) {
+function onTypeSelected(selected: QuickItem): void {
     console.log(selected);
 }
 
-function typesToItems(types: RawTypeDefinition[]) : QuickItem[] {
+function typeToQuickItem(types: RawTypeDefinition[]): QuickItem[] {
     return types
     .map((type) => ({ description: type.l, label: type.t, detail: type.p } as QuickItem))
-    .sort((a, b) => {
-        if(a.label < b.label) return -1;
-        if(a.label > b.label) return 1;
-        return 0;
-    });
+    .sort((a, b) => a.label < b.label ? -1 : a.label > b.label ? 1 : 0);
 }
 
-async function onCommand() {
-    const selected = await vscode.window.showQuickPick(typesToItems(types), { placeHolder });
+async function onCommand(): Promise<void> {
+    const selected = await vscode.window.showQuickPick(typeToQuickItem(types), { placeHolder });
     onTypeSelected(selected);
 }
 
