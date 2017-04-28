4f093d317b051f9c7615b1b4396fc974d1d6c23f
Lucas <lucas.schejtman@gmail.com>
2017-04-28 13:07:55 +1200

    added config

---
+++ generator-shell/app/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    prompts: [
+        {
+            type: 'input',
+            name: 'name',
+            message: 'Your component name',
+            default: 'shell-component'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Your component description',
+            default: 'awesome piece of react'
+        },
+        {
+            type: 'input',
+            name: 'version',
+            message: 'Your component version',
+            default: '0.1.0'
+        },
+        {
+            type: 'input',
+            name: 'repository',
+            message: 'Your component repository',
+            // assuming it's a git repository
+            filter: (value) => value && value !== '' && !value.endsWith('.git') ? `${value}.git` : value
+        },
+        {
+            type: 'checkbox',
+            name: 'deps',
+            message: 'Select runtime dependencies',
+            choices: [
+                { name: 'redux', value: { "redux" : "*" } },
+                { name: 'react', value: { "react" : "*" } }
+            ],
+            // merge all selected options into an object
+            filter: (value) => value.reduce((acc, val) => Object.assign({}, acc, val), {})
+        }
+    ]
+};
