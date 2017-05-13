8ddcf0392d136104b5ca72e1d605343b066baf2d
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-05-13 13:32:42 +1200

    package manager option

--- generator-shell/app/config/options.js
+++ generator-shell/app/config/options.js
@@ -5,5 +5,11 @@ module.exports = [
             default: false,
             type: v => v && v.toString().toLowerCase() === 'true'
         }
+    },
+    {
+        name: 'pkgmanager',
+        config: {
+            default: 'npm'
+        }
     }
 ];
