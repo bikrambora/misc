ad18b6c0cdbd61dda5c316aaee7c63829961a229
Lucas <lucas.schejtman@gmail.com>
2017-05-16 13:17:26 +1200

    added skipinstall option description

--- generator-shell/app/config/options.js
+++ generator-shell/app/config/options.js
@@ -3,7 +3,8 @@ module.exports = [
         name: 'skipinstall',
         config: {
             default: false,
-            type: v => v && v.toString().toLowerCase() === 'true'
+            type: v => v && v.toString().toLowerCase() === 'true',
+            desc: 'Determines whether to install dependencies once the generator is done'
         }
     },
     {
