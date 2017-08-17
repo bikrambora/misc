e5981cd5f4c1318448db68ca42cd4a770f119cf3
Lucas <lucas.schejtman@gmail.com>
2017-08-18 10:42:19 +1200

    encodeRandom

--- ulid/Program.fs
+++ ulid/Program.fs
@@ -11,6 +11,11 @@ let (|Positive|NotPositive|) num =
 let concatEncoding chars =
     List.fold (fun acc char -> acc + (encoding.Chars char).ToString()) "" chars
 
+let encodeRandom length =
+    let rnd = System.Random();
+    [1..length]
+        |> List.map(fun _ -> rnd.Next(1, 32))
+
 let encodeTime timestamp length =
     let rec loop ts len chars =
         match len with
@@ -23,7 +28,10 @@ let encodeTime timestamp length =
 
 [<EntryPoint>]
 let main argv =
-    [(1470118279201UL, 8); (1469918176385UL, 10); (1470264322240UL, 12)]
-        |> List.map ((fun (ts, len) -> encodeTime ts len) >> concatEncoding)
-        |> List.iter (printfn "%s")
+    let q = [(1470118279201UL, 8); (1469918176385UL, 10); (1470264322240UL, 12)]
+                |> List.map ((fun (ts, len) -> encodeTime ts len) >> concatEncoding)
+                //|> List.iter (printfn "%s")
+                |> List.head
+    let ran = encodeRandom 8 |> concatEncoding
+    printfn "%s" (q + ran)
     0
