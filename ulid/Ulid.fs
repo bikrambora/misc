2d2caf88f9e23d43ebcaf1cf931a9c23cb003e97
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-08-25 10:51:39 +1200

    cleanup files

---
+++ ulid/Ulid.fs
@@ -0,0 +1,44 @@
+namespace Ulid
+
+open Ulid.Extensions
+
+open System
+
+[<AutoOpen>]
+module Ulid =
+    let encoding        = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+    let encodingLength  = 32L
+
+    let (|Positive|NotPositive|) num =
+        match num > 0 with
+        | true -> Positive
+        | _    -> NotPositive
+
+    let concatEncoding chars =
+        List.fold (fun acc char -> acc + (encoding.Chars char).ToString()) "" chars
+
+    let encodeRandom length =
+        let rnd = Random();
+        List.init length (fun _ -> rnd.Next(0, 31))
+
+    let encodeTime timestamp length =
+        let rec loop ts len chars =
+            match len with
+            | Positive    -> let char = ts % encodingLength
+                             let acc = (ts - char) / encodingLength
+                             loop acc (len - 1) ((Convert.ToInt32 char)::chars)
+            | NotPositive -> chars
+
+        loop timestamp length []
+
+    type Ulid =
+        static member private Generate timestamp =
+            let time = encodeTime timestamp 10 |> concatEncoding
+            let rnd  = encodeRandom 16 |> concatEncoding
+            time + rnd
+
+        static member FromTimestamp timestamp =
+            Ulid.Generate timestamp
+
+        static member New =
+            Ulid.Generate DateTime.UnixTime
---
+++ ulid/zProgram.fs
@@ -0,0 +1,8 @@
+﻿open Ulid
+open System
+
+[<EntryPoint>]
+let main argv =
+    printfn "%s" (Ulid.New)
+    printfn "%s" (Ulid.FromTimestamp 1469918176385L)
+    0
