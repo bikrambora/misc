7165e894a506acbf5329e0fedea9531d0ce48106
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-09-13 12:08:01 +1200

    playing with fsi and async

--- typeproviders/Script.fsx
+++ typeproviders/Script.fsx
@@ -2,9 +2,20 @@
 
 open FSharp.Data
 
-let data = WorldBankData.GetDataContext()
+// type json = JsonProvider<"""[{ "name": "l" }, { "name": 23 }]""">
 
-data
-    .Countries.``Argentina``
-    .Indicators.``Account, primary education or less (% ages 15+)``
-    |> Seq.head
+// let j = json.GetSamples() |> Seq.last
+
+// match j.Name.Number, j.Name.String with
+//     | Some num, _ -> printfn "%d" (num + 1)
+//     | _, Some str -> printfn "%s" ("Hello " + str)
+//     | _, _ -> printfn "No matching type"
+
+// type Post = JsonProvider<"""{ "userId": 1, "id": 1, "title": "string", "body": "string"}""">
+async {
+    //System.Net.ServicePointManager.ServerCertificateValidationCallback += (o, certificate, chain, errors) => true;
+    let req = System.Net.WebRequest.Create("https://jsonplaceholder.typicode.com/posts/1")
+    let! res = req.AsyncGetResponse()
+    printfn "%d" res.ContentLength
+} |> Async.RunSynchronously
+System.Console.ReadLine()
