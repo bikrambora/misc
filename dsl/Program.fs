12dd923dba3099b307448162f971a6ed017e5ffb
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-09-16 11:40:21 +1200

    initial project

---
+++ dsl/Program.fs
@@ -0,0 +1,12 @@
+﻿open FParsec
+open System
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(error, _, _) -> printfn "Failure: %s" error
+
+[<EntryPoint>]
+let main argv =
+    test pfloat "3.2"
+    0
