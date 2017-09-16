4d1079e189acc8122c6f0f7f327e24ce3836ba6f
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-09-17 10:49:01 +1200

    different parsers

--- dsl/Program.fs
+++ dsl/Program.fs
@@ -4,9 +4,10 @@ open System
 let test p str =
     match run p str with
     | Success(result, _, _)   -> printfn "Success: %A" result
-    | Failure(error, _, _) -> printfn "Failure: %s" error
+    | Failure(error, _, _) -> printfn "Failure: %A" error
 
 [<EntryPoint>]
 let main argv =
     test pfloat "3.2"
+    test pint32 "3.4"
     0
