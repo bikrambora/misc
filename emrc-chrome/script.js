f7f1ab1b2d046055d02ca6c30b169713a73136a4
Lucas <lucas.schejtman@gmail.com>
2017-10-24 10:54:13 +1300

    clean up

--- emrc-chrome/script.js
+++ emrc-chrome/script.js
@@ -1,17 +1,21 @@
 var ENDPOINT = 'http://localhost:3000/context/';
 var ELEMENT_ID = 'VisibleID';
+var POLL_INTERVAL = 500;
 var element = document.getElementById(ELEMENT_ID);
 var oldValue = element.value;
+
+function setContext(id) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', ENDPOINT + id, true);
+    xhr.send();
+}
 
 function onInput() {
     var newValue = element.value;
     if(oldValue === newValue) return;
 
     oldValue = newValue;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', ENDPOINT + newValue, true);
-    xhr.send();
+    setContext(newValue);
 }
 
-setInterval(onInput, 500);
+setInterval(onInput, POLL_INTERVAL);
