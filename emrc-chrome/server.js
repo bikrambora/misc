99d3d6355e209da9e4fcb368a8cc31d041ec63b5
Lucas <lucas.schejtman@gmail.com>
2017-10-24 11:36:15 +1300

    more cleanup

--- emrc-chrome/server.js
+++ emrc-chrome/server.js
@@ -5,12 +5,12 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/context/:pid", function(req, res) {
-    console.log(req.params.pid);
+app.get("/context/:id", function(req, res) {
+    console.log("Received id: %s", req.params.id);
     res.send();
 })
 
 
 var server = app.listen(3000, function () {
-    console.log("Listening on port %s...", server.address().port);
+    console.log("Listening on port %s", server.address().port);
 });
