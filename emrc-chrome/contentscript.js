f711541f410ec3102a469eefe1bfafe7906d1e0d
Lucas <lucas.schejtman@gmail.com>
2017-10-25 11:14:30 +1300

    not polute global scope

--- emrc-chrome/contentscript.js
+++ emrc-chrome/contentscript.js
@@ -1,9 +1,6 @@
-var s = document.createElement('script');
-s.src = chrome.extension.getURL('script.js');
-s.onload = function() {
+var script = document.createElement('script');
+script.onload = function() {
     this.remove();
 };
-console.log('content script');
-(document.head || document.documentElement).appendChild(s);
-//console.log(chrome);
-//chrome.tabs.executeScript(null, {file: "script.js"});
+script.src = chrome.extension.getURL('script.js');
+(document.head || document.documentElement).appendChild(script);
--- emrc-chrome/script.js
+++ emrc-chrome/script.js
@@ -1,21 +1,32 @@
-var ENDPOINT = 'http://localhost:3000/context/';
-var ELEMENT_ID = 'VisibleID';
-var POLL_INTERVAL = 500;
-var element = document.getElementById(ELEMENT_ID);
-var oldValue = element.value;
+(function EMRC_PLUGIN(opts) {
+    var misConfigured = function() {
+        alert('Misconfigured plugin, check your settings');
+    }
 
-function setContext(id) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', ENDPOINT + id, true);
-    xhr.send();
-}
+    var ENDPOINT = opts.endpoint || misConfigured();
+    var ELEMENT_ID = opts.elementId || misConfigured();
+    var POLL_INTERVAL = opts.pollInterval || 3000;
 
-function onInput() {
-    var newValue = element.value;
-    if(oldValue === newValue) return;
+    var element = document.getElementById(ELEMENT_ID);
+    var oldValue = element.value;
 
-    oldValue = newValue;
-    setContext(newValue);
-}
+    function setContext(id) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', ENDPOINT + id, true);
+        xhr.send();
+    }
 
-setInterval(onInput, POLL_INTERVAL);
+    function onTick() {
+        var newValue = element.value;
+        if(oldValue === newValue) return;
+
+        oldValue = newValue;
+        setContext(newValue);
+    }
+
+    setInterval(onTick, POLL_INTERVAL);
+})({
+    endpoint: 'http://localhost:3000/context/',
+    elementId: 'VisibleID',
+    pollInterval: 500
+})
