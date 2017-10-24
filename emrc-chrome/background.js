3a5621561f5e0894fcdb3fc3557e7e8416193406
Lucas <lucas.schejtman@gmail.com>
2017-10-25 10:52:14 +1300

    removed unused icons, cleaned scripts

--- emrc-chrome/background.js
+++ emrc-chrome/background.js
@@ -3,4 +3,5 @@ s.src = chrome.extension.getURL('script.js');
 s.onload = function() {
     this.remove();
 };
+console.log('background script');
 (document.head || document.documentElement).appendChild(s);
--- emrc-chrome/contentscript.js
+++ emrc-chrome/contentscript.js
@@ -3,4 +3,7 @@ s.src = chrome.extension.getURL('script.js');
 s.onload = function() {
     this.remove();
 };
+console.log('content script');
 (document.head || document.documentElement).appendChild(s);
+//console.log(chrome);
+//chrome.tabs.executeScript(null, {file: "script.js"});
