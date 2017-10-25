6646b549fefdc62070b1581509db1668086b71b4
Lucas <lucas.schejtman@gmail.com>
2017-10-26 10:58:30 +1300

    showing some chrome API capabilities

--- emrc-chrome/contentscript.js
+++ emrc-chrome/contentscript.js
@@ -1,5 +1,22 @@
 var script = document.createElement('script');
-script.onload = function() {
+script.onload = function(e) {
+    // If we want to manage its lifecycle
+    // Right permissions will need to be set
+    // var conn = chrome.runtime.connect({ name: 'emrc_connection' });
+    // conn.postMessage({
+    //     name: 'INIT',
+    //     payload: {
+    //         endpoint: 'http://localhost:3000/context/',
+    //         elementId: 'VisibleID',
+    //         pollInterval: 500
+    //     }
+    // });
+    // conn.postMessage({
+    //     name: 'START'
+    // });
+    // conn.postMessage({
+    //     name: 'STOP'
+    // });
     this.remove();
 };
 script.src = chrome.extension.getURL('script.js');
