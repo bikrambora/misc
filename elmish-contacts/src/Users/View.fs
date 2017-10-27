c48eccafce75fbe61cc14c66003b110f111881b4
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-10-28 11:44:19 +1300

    starting to play with table bindings

--- elmish-contacts/src/Users/View.fs
+++ elmish-contacts/src/Users/View.fs
@@ -2,7 +2,7 @@ module User.View
 
 open Elmish.React
 module R = Fable.Helpers.React
-//open Fable.Helpers.React.Props
+open Fable.Helpers.React.Props
 open Types
 
 let placeholder msg =
@@ -16,8 +16,16 @@ let viewPlaceholder msg =
         lazyView placeholder msg
     ]
 
+let tableRow xs =
+    R.tr[] [ for x in xs -> R.td [] [R.str x.name] ]
+
 let viewUsernames users =
-    R.section [] [
-        R.ul []
-            (users |> List.map viewUsername)
-    ]
+    R.table [ ClassName "table" ]
+        [
+            R.thead [] [ R.td [] [R.str "Name"]];
+            R.tbody [] [ tableRow users ]
+        ]
+    // R.section [] [
+    //     R.ul []
+    //         (users |> List.map viewUsername)
+    // ]
