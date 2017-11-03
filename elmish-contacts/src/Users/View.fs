e43df159bcdb01f83c13055bbd41b1917b85bd12
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-11-04 12:03:58 +1300

    added company

--- elmish-contacts/src/Users/View.fs
+++ elmish-contacts/src/Users/View.fs
@@ -19,14 +19,16 @@ let viewPlaceholder msg =
 let tableHeaders() =
     R.tr []
         [R.td [] [R.str "Name"]
-         R.td [] [R.str "Email"]]
+         R.td [] [R.str "Email"]
+         R.td [] [R.str "Company"]]
 
 let viewUsers =
     List.map
         (fun x ->
             R.tr []
                 [R.td [] [R.str x.name]
-                 R.td [] [R.str x.email]])
+                 R.td [] [R.str x.email]
+                 R.td [] [R.str x.company.name]])
 
 let viewUsernames users =
     R.table
