e3f532e3456a2ae20b82ef51782715fbe1e28dfb
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-11-07 11:48:26 +1300

    added city

--- elmish-contacts/src/Users/View.fs
+++ elmish-contacts/src/Users/View.fs
@@ -21,7 +21,8 @@ let tableHeaders() =
         [R.td [] [R.str "Name"]
          R.td [] [R.str "Email"]
          R.td [] [R.str "Company"]
-         R.td [] [R.str "Username"]]
+         R.td [] [R.str "Username"]
+         R.td [] [R.str "City"]]
 
 let viewUsers =
     List.map
@@ -30,7 +31,8 @@ let viewUsers =
                 [R.td [] [R.str x.name]
                  R.td [] [R.str x.email]
                  R.td [] [R.str x.company.name]
-                 R.td [] [R.str x.username]])
+                 R.td [] [R.str x.username]
+                 R.td [] [R.str x.address.city]])
 
 let viewUsernames users =
     R.table
