5230df568411082f025530d6e6fdf004a24b2797
Lucas Schejtman <lucas.schejtman@gmail.com>
2017-11-14 09:21:03 +1300

    cleanup navbar

--- elmish-contacts/src/Navbar/View.fs
+++ elmish-contacts/src/Navbar/View.fs
@@ -3,30 +3,6 @@ module Navbar.View
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
-let navButton classy href faClass txt =
-    p
-        [ ClassName "control" ]
-        [ a
-            [ ClassName (sprintf "button %s" classy)
-              Href href ]
-            [ span
-                [ ClassName "icon" ]
-                [ i
-                    [ ClassName (sprintf "fa %s" faClass) ]
-                    [ ] ]
-              span
-                [ ]
-                [ str txt ] ] ]
-
-let navButtons =
-    span
-        [ ClassName "nav-item" ]
-        [ div
-            [ ClassName "field is-grouped" ]
-            [ navButton "twitter" "https://twitter.com/FableCompiler" "fa-twitter" "Twitter"
-              navButton "github" "https://github.com/fable-compiler/fable-elmish" "fa-github" "Fork me"
-              navButton "github" "https://gitter.im/fable-compiler/Fable" "fa-comments" "Gitter" ] ]
-
 let root =
     nav
         [ ClassName "nav" ]
@@ -37,4 +13,4 @@ let root =
                 [ str "Elmish" ] ]
           div
             [ ClassName "nav-right" ]
-            [ navButtons ] ]
+            [] ]
